b98d240118fde1b5c650e58f102a418ac4ad32bf
JaydenDev <willsjayden2@gmail.com>
2022-10-23 16:43:51 -0500

    chore: init

---
+++ applicards.js
@@ -0,0 +1,18 @@
+function generateCards() {
+    fetch('./cards.json')
+        .then((response) => response.json())
+        .then((data) => {
+            const body = document.body;
+            for (const [slideId, slide, id, bgcolor, textcolor, font] of Object.entries(data.slides)) {
+                const elem = document.createElement('div');
+                elem.style = 'height: 100vh;'
+                elem.innerText = slide.content;
+                elem.id = slide.id;
+                elem.style.backgroundColor = slide.bgcolor;
+                elem.style.color = slide.textcolor;
+                elem.style.fontFamily = slide.font
+                body.appendChild(elem);
+            }
+        });
+}
+window.onload = generateCards;
